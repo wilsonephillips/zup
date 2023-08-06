207c2d2f4dfa62602321a5a2bbe16d8773f9fce9
Wilson Phillips <88855916+wilsonephillips@users.noreply.github.com>
2023-08-06 07:39:17 -0500

    Update zup.sh

--- zup.sh
+++ zup.sh
@@ -9,16 +9,20 @@
 
 # added color to the echo lines
 
+echo
 echo -e "\033[1;31m****Updating all System packages****\033[0m" "\n" # the "\n" adds a blank line
 sudo zypper dist-upgrade
+echo
 echo -e "\033[1;31m****Updating Flatpaks from FlatHub****\033[0m" "\n" # the "\n" adds a blank line
 flatpak update
+echo
 echo -e "\033[1;32m****Script Completed!****\033[0m" "\n" # the "\n" adds a blank line
 
 #This keeps the window open till you are sure you
 #you are ready to close it. Comment this out and
 #it will close automatically.
 
+echo
 read -n 1 -r -s -p "Press any key to EXIT"
 echo "\ncool"
 exit 0;
@@ -30,20 +34,20 @@ exit 0;
 # Regular Colors
 
 # Value    | Color
-# -------- | ------
+# -------- | -----
 # \e[0;30m | Black
-# \e[0;31m | Red    |
-# \e[0;32m | Green  |
-# \e[0;33m | Yellow |
-# \e[0;34m | Blue   |
-# \e[0;35m | Purple |
-# \e[0;36m | Cyan   |
-# \e[0;37m | White  |
+# \e[0;31m | Red
+# \e[0;32m | Green
+# \e[0;33m | Yellow
+# \e[0;34m | Blue
+# \e[0;35m | Purple
+# \e[0;36m | Cyan
+# \e[0;37m | White
 
 # Bold
 
 # Value    | Color
-# -------- | ------
+# -------- | -----
 # \e[1;30m | Bright Black
 # \e[1;31m | Bright Red
 # \e[1;32m | Bright Green
